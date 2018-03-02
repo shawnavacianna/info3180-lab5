751e83b04e9cef4fca65f86255eead57fd8ad0ee
ShawnaVacianna <shawnkay.vacianna@mymona.uwi.edu>
2018-03-02 23:12:06 +0000

    step 8, 9 10 completed

--- app/views.py
+++ app/views.py
@@ -34,21 +34,43 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            user = UserProfile.query.filter_by(username=username).first()
+           
+            
+            if user.password ==password:
+                 # get user id, load into session
+                 login_user(user)
+                 
+                 next_page = request.args.get('next')
+                  # remember to flash a message to the user
+                 flash("Login Successful", "success")
+                 print next_page
+                 return redirect(next_page or url_for("secure"))  # they should be redirected to a secure-page route instead
+            
+            flash("login failed", 'danger')
+            
     return render_template("login.html", form=form)
-
+    
+@app.route('/secure-page')
+@login_required
+def secure():
+    return render_template('secure_page.html')
+    
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    
+    flash('Logged out', 'success')
+    return redirect(url_for("home"))
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
